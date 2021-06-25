a14cbeb42c1fb8ee847f405ac9481a007b11cab6
Kyle Le <kle@kle.remote.csb>
2021-06-25 15:37:53 -0700

    Testing

--- src/main/java/com/redhat/dev/config/Config.java
+++ src/main/java/com/redhat/dev/config/Config.java
@@ -1,19 +1,20 @@
-package com.example.restservice;
+package com.redhat.dev.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "user")
-public class EnvReader {
-	
-	private String env;
-	
+@ConfigurationProperties(prefix = "user" )
+public class Config {
+
+    private String env;
+
 	public String getEnv() {
 		return env;
 	}
-	
+
 	public void setEnv(String env) {
 		this.env = env;
 	}
+
 }
