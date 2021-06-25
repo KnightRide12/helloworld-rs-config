71ad194550f1c5373539254756ba2f9aadde0cc0
Kyle Le <kle@kle.remote.csb>
2021-06-25 14:39:38 -0700

    Testing

--- src/main/java/com/example/restservice/EnvReader.java
+++ src/main/java/com/example/restservice/EnvReader.java
@@ -1,13 +1,19 @@
 package com.example.restservice;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ConfigurationProperties(prefix = "user")
 public class EnvReader {
 	
-	@Value("${user.env}")
 	private String env;
 	
 	public String getEnv() {
 		return env;
+	}
+	
+	public void setEnv(String env) {
+		this.env = env;
 	}
 }
